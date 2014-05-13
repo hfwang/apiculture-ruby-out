359240193c36148d6076664386d2a1d8580e3073
Hsiu-Fan Wang <hfwang@porkbuns.net>
2014-05-12 19:30:46 -0700

    Tweak template options a bit

--- init.rb
+++ init.rb
@@ -1,7 +1,11 @@
 Apiculture.register_template do |builder|
   builder.configure_options do |template_options|
-    template_options["gem_name"] = ask("Gem name? (in UpperSnakeCase)",
+    template_options["gem_name"] = ask("Gem name? (in underscore_case)",
                                        :default => template_options["gem_name"])
+    template_options["module_name"] = template_options["module_name"] ||
+      Apiculture::StringUtils.camelize(template_options["gem_name"])
+    template_options["module_name"] = ask("Module name? (in UpperCamelCase)",
+                                          :default => template_options["module_name"])
   end
 
   builder.generate do |outputter, destination|
